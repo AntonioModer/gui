8658a1ae86305f11049c766e1aff7d5021a1d6de
Starkkz <matiasahermosilla@gmail.com>
2016-06-03 19:35:08 -0300

    Updated progressbar layout

--- skins/cyan/progressbar.lua
+++ skins/cyan/progressbar.lua
@@ -6,7 +6,13 @@ for y = 0, 19 do
 	Progress.Image:setPixel(0, y, 255 * 9/11 + 255 * (19 - y) * 2/11 * 1/19, 255 * 9/11 + 255 * (19 - y) * 2/11 * 1/19, 255 * 9/11 + 255 * (19 - y) * 2/11 * 1/19)
 end
 Progress.Image = love.graphics.newImage(Progress.Image)
+Progress.ImageColor = {80, 180, 255, 255}
+
 Progress.TextFont = love.graphics.newFont(gui.Fonts["Kanit Light"], 13)
+Progress.TextColor = {80, 80, 80, 255}
+
+Progress.BorderColor = {80, 80, 80, 255}
+Progress.BackgroundColor = {220, 220, 220, 255}
 
 local function DrawProgress()
 	if Progress.Width > 0 then
@@ -14,25 +20,33 @@ local function DrawProgress()
 	end
 end
 
+function Progress:Init()
+	self.Layout.ImageColor = Progress.ImageColor
+	self.Layout.TextFont = Progress.TextFont
+	self.Layout.TextColor = Progress.TextColor
+	self.Layout.BorderColor = Progress.BorderColor
+	self.Layout.BackgroundColor = Progress.BackgroundColor
+end
+
 function Progress:Render(dt)
 	local Width, Height = self:GetDimensions()
 	Progress.Width, Progress.Height = Width * self.Progress, Height
 	
-	love.graphics.setColor(100, 100, 100, 255)
+	love.graphics.setColor(self.Layout.BorderColor)
 	gui.graphics.roundedbox("line", 4, 1, 1, Width - 2, Height - 2)
 	
-	love.graphics.setColor(220, 220, 220, 255)
+	love.graphics.setColor(self.Layout.BackgroundColor)
 	gui.graphics.roundedbox("fill", 4, 1, 1, Width - 2, Height - 2)
 	
 	love.graphics.stencil(DrawProgress)
 	love.graphics.setStencilTest("greater", 0)
 	
-	love.graphics.setColor(80, 180, 255, 255)
+	love.graphics.setColor(self.Layout.ImageColor)
 	love.graphics.draw(Progress.Image, 1, 1, 0, (Width - 2), (Height - 2)/20)
 	love.graphics.setStencilTest()
 	
-	love.graphics.setColor(80, 80, 80, 255)
-	love.graphics.setFont(Progress.TextFont)
+	love.graphics.setColor(self.Layout.TextColor)
+	love.graphics.setFont(self.Layout.TextFont)
 	
 	local Text = (self.Progress * 100).." %"
 	love.graphics.print(Text,  math.floor((Width - Progress.TextFont:getWidth(Text))/2), math.floor((Height - Progress.TextFont:getHeight(Text))/2))
